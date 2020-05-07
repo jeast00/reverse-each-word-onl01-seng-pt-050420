39baf124d35e2e11902e2ec52bc2b52e0dfe87e5
Jonathan East <jonathan_east@ymail.com>
2020-05-07 21:01:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
- sentence_array = sentence.split()
+ sentence_array = sentence.split(" ")
+ reversed_word = []
  sentence_array.each do |word|
-   word
+   reversed_word << word.reverse
  end
 end
